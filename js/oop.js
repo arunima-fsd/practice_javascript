805787e4a9cfe87cfbb9a611eee8e71da9f641fe
Arunima Srivastava <arunimaasrivastava@gmail.com>
2017-01-26 00:01:11 +0530

    More on prototype.

--- js/oop.js
+++ js/oop.js
@@ -1,3 +1,4 @@
+
 console.log("****************THE THIS KEYWORD*************************");
 
 "use strict";
@@ -52,13 +53,136 @@ r.my_method.call(y, g, b);
 //setTimeout(func(y, g), 1000);
 
 //This is the way which in which we can call func()
-setTimeout(function () {
-    r.my_method(y, g);
-
-}, 1000);
+//setTimeout(function () {r.my_method(y, g);}, 1000);
 
 console.log(this);
 
 new r.my_method(g, b);
+
+
+console.log("****************PROTOTYPE CHAINING*************************");
+
+//Following is a object literal
+
+var gold = { type: "precious" };
+var ornament = Object.create(gold);
+ornament.type = "jewellery";
+
+
+console.log(gold.isPrototypeOf(ornament));
+console.log(Function.prototype.isPrototypeOf(Object));
+console.log(Object.prototype.isPrototypeOf(gold));
+
+console.log(gold instanceof Object);
+console.log(Function instanceof Object);
+console.log(Object instanceof Function);
+//console.log(Object instanceof gold);
+
+console.log(Object.getPrototypeOf(gold));
+
+
+
+
+/*All objects will have a constructor property.  Objects created without
+the explicit use of a constructor function (i.e. the object and array literals)
+will have a constructor property that points to the Fundamental Object constructor
+ type for that object e.g. Array, Number, Object.
+ */
+
+// .constructor Returns a reference to the Object constructor function that
+// created the instance object.
+console.log(ornament.constructor);
+
+
+// gold has inherited from Object.prototype and ornament has inherited
+// from gold (not gold.prototype). Since, gold is a object literal
+// not a constructor function. Only constructor function has a
+//property .prototype
+
+console.log(Object.getPrototypeOf({}) === Object.prototype);
+
+console.log(Object.getPrototypeOf(gold) === Object.prototype);
+
+console.log(Object.getPrototypeOf(ornament) === gold);
+
+console.log(Object.getPrototypeOf(Object.prototype))
+
+console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
+
+
+var myArr = [];
+
+console.log(myArr.constructor);
+
+console.log("************PSEUDOCLASSICAL Pattern *****************");
+
+/* One question arises, What is the need of storing all the methods inside
+* the property methid of a constructor function? The answer is to save memory.
+* Otherwise, if we declare the sayHello() inside the Dog function then everytime
+* an object is created, large amount of space is dedicated to save the method.
+* But, in this case the function is saved at one place and whenver called it will
+* be looked upon on Dog.prototype. */
+
+
+
+function Dog(name, age, breed) {
+    //this = Object.create(Dog.prototype);
+    //the above line is automatically inserted by the intepreter when
+    //the instance is made with new keyword.
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    //return this.
+}
+
+Dog.prototype.sayHello = function () {
+                                    console.log(this.name + " the "+ this.breed+", says woof woof!!!");
+                                        };
+
+var guddu = new Dog("Guddu", 2, "Pug");
+guddu.sayHello();
+
+
+
+
+
+console.log("*****************FUNCTIONAL Class ***********************************");
+//These creates the instances of a particular class. Like
+//Constructor function.
+
+var Car = function ( loc ) {
+    var obj = {
+        loc: loc,
+        move: function () {
+            obj.loc ++;
+
+        }
+    };
+
+    return obj;
+}
+
+var amy = Car(1);
+//amy.move();
+var ben = Car(9);
+//ben.move();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
